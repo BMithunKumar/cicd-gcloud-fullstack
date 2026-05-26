488bc6ce5c3021f4d76e0fada97adda3f129c85d
BMithunKumar <mithunb@genworx.ai>
2026-05-26 14:52:51 +0530

    some changes

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "This is home page"
+
+@app.get("/payment")
+def payment():
+    return "This is payment page"
+
+
+@app.post("/calculation")
+def calculation(a,b):
+    return a+b
