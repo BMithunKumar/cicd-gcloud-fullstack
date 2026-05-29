2e3a249fda290016c129260ac2f1693800623718
BMithunKumar <mithunb@genworx.ai>
2026-05-29 10:50:04 +0530

    total frontend backend workflow with smoke test

--- backend/test_main.py
+++ backend/test_main.py
@@ -7,7 +7,6 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
 
-
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
