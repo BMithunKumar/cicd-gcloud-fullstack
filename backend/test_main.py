05fca328e54d2357c2e5bbb3db4cd9bac86fc616
BMithunKumar <mithunb@genworx.ai>
2026-05-29 10:20:54 +0530

    backend cicd try

--- backend/test_main.py
+++ backend/test_main.py
@@ -7,6 +7,7 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
